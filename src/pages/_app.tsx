b3c35fc79bb9cab86fa027d1d580001ce687d123
Jin <t19887348@hotmail.com>
2022-05-10 21:59:37 +1000

    feat: theme setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import store from "../store";
 import { Chakra } from "@/styles/Chakra";
 import Layout from "@/layouts";
+import "@/styles/font.css";
 
 function CourtCanvaApp({ Component, pageProps }: AppProps) {
   return (
