09be3b3206e5b947be045efe5ef19a7d8e1f3bda
Jin <t19887348@hotmail.com>
2022-05-10 21:59:37 +1000

    feat: theme setting

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,4 +1,4 @@
-import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { extendTheme, ThemeConfig, theme as chakraTheme } from "@chakra-ui/react";
 
 const config: ThemeConfig = {
   initialColorMode: "light",
@@ -16,14 +16,53 @@ const styles = {
   },
 };
 
+const fonts = {
+  heading: `Oswald, ${chakraTheme.fonts?.heading}`,
+  body: `Oswald, ${chakraTheme.fonts?.heading}`,
+};
+
 const colors = {
   brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
+    primary: "#344C5C",
+    secondary: "#40B484",
+  },
+  state: {
+    info: "#A6D0C6",
+    success: "#39A66B",
+    warning: "#E59934",
+    error: "#BF1351",
+  },
+  black: {
+    300: "#000",
+    200: "#1D1D1D",
+    100: "#282828",
+  },
+  grey: {
+    500: "#333",
+    400: "#4F4F4F",
+    300: "#828282",
+    200: "#BDBDBD",
+    100: "#DEEEEA",
   },
 };
 
-const theme = extendTheme(styles, config, colors);
+const fontSize = {
+  xs: "12px",
+  sm: "14px",
+  md: "16px",
+  lg: "18px",
+  xl: "20px",
+};
+
+const overrides = {
+  ...chakraTheme,
+  fonts,
+  colors,
+  config,
+  styles,
+  fontSize,
+};
+
+const theme = extendTheme(overrides);
 
 export default theme;
