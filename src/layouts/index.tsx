233c7f218919d821ca79f07201670172044e7e9e
Emily <mei.gong@icloud.com>
2022-05-12 14:06:40 +1000

    feat: add responsibility to navigation bar

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { Box } from "@chakra-ui/react";
 import Header from "./Header";
 import Footer from "./Footer";
-import NavigationBar from "../components/NavBar/NavigationBar";
+import NavigationBar from "../components/NavBar";
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
