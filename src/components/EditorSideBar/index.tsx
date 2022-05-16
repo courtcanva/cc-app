a65d86f3d5113c9958bf5f6e83723706c58bf5e5
JasonCaoAU <jason2019au@gmail.com>
2022-05-16 22:22:11 +1000

    refactor: cc-0006 update the height of sidebar and position

--- src/components/EditorSideBar/index.tsx
+++ src/components/EditorSideBar/index.tsx
@@ -4,7 +4,7 @@ import { Box, Flex } from "@chakra-ui/react";
 
 const EditorSideBar = () => {
   return (
-    <Box bg="background.primary" w="98px" h="calc(100vh)" position="fixed" top="71px" left="0">
+    <Box bg="background.primary" w="98px" h="100vh" position="fixed" top="72px" left="0">
       <Flex align="center" justify="center" flexDirection="column" maxW="98px">
         {sideBarItemList.map((item) => (
           <EditorSideBarItem key={item.title} title={item.title} icon={item.icon} />
