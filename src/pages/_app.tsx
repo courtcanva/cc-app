e539fcac1c6a5f41abd072d5d4c2e4a2ad631168
Tato <jason.ma1904@gmail.com>
2022-05-18 21:54:52 +1000

    fix: removed duplicate chakra provider import as this is already impo… (#18)
    fix: removed duplicate chakra provider import as this is already imported from styles/chakra file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import store from "../store";
 import { Chakra } from "@/styles/Chakra";
 import Layout from "@/layouts";
-import { ChakraProvider } from "@chakra-ui/react";
 
 function CourtCanvaApp({ Component, pageProps }: AppProps) {
   return (
