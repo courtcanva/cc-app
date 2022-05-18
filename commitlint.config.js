f421e8e10f22b514c11ac0a5e90d977e3006e05c
sunny <49833687+sunnyyangyyc@users.noreply.github.com>
2022-05-18 16:17:56 +1000

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [
-      2,
+      1,
       "always",
       ["ci", "chore", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test"],
     ],
