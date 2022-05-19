bc104c8de64e9e40c01584f9e88acdcc7da58586
StevenLiu <stevenliu986@gmail.com>
2022-05-19 10:13:33 +1000

    fix: modify png names & their descriptions

--- src/components/ChangeCourtSize/CourtList.tsx
+++ src/components/ChangeCourtSize/CourtList.tsx
@@ -1,32 +1,33 @@
 const courtList = [
   {
-    des: "510 m2 Pro full court",
-    des1: "(17 m* 30 m)",
+    des: "510 ㎡ Pro Court",
+    des1: "(17 m * 30 m)",
     img: "./courtSize/court-510m2.png",
   },
   {
-    des: "420 m2 Medium full court",
-    des1: "(15 m* 28 m)",
+    des: "420 ㎡ Full Court",
+    des1: "(15 m * 28 m)",
     img: "./courtSize/court-420m2.png",
   },
   {
-    des: "45 m2 Small full court",
-    des1: "(5 m* 9 m)",
+    des: "45 ㎡ Small Court",
+    des1: "(5 m * 9 m)",
     img: "./courtSize/court-45m2.png",
   },
   {
-    des: "210 m2 Pro half court",
-    des1: "(15 m* 14 m)",
+    des: "210 ㎡ Pro Half Court",
+    des1: "(15 m * 14 m)",
     img: "./courtSize/court-210m2.png",
   },
   {
-    des: "150 m2 Half court",
-    des1: "(15 m* 28 m)",
+    des: "150 ㎡ Half Court",
+    des1: "(15 m * 28 m)",
     img: "./courtSize/court-150m2.png",
   },
   {
-    des: "Medium half court",
-    img: "./courtSize/court-Meduim-half.png",
+    des: "70 ㎡ Medium Court",
+    des1: "(7 m * 10 m)",
+    img: "./courtSize/court-70m2.png",
   },
 ];
 export default courtList;
