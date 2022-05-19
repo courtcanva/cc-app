8d69a5a3d2aa4636d959ffae420fc5165283f090
Emily <mei.gong@icloud.com>
2022-05-19 16:27:27 +1000

    feat: cc-0015 add switch on-off function and test

---
+++ src/__tests__/components/EditorFooter/index.test.tsx
@@ -0,0 +1,40 @@
+import {fireEvent, render, screen } from "@testing-library/react";
+import EditorFooter from "../../../components/EditorFooter";
+
+describe("EditorFooter", () =>{
+  it("should see the switch button",()=>{
+    render (<EditorFooter />);
+    const switchButton = screen.getByTestId("switch-btn");
+    expect(switchButton).toBeInTheDocument();
+  });
+  it("should toggle between on and off",()=>{
+    const {getByTestId} =render (<EditorFooter />);
+    const toggleBtn = getByTestId("switch-btn")
+    fireEvent.click(toggleBtn)
+    expect(getByTestId("ruler-label")).toHaveTextContent("Ruler on")
+  fireEvent.click(toggleBtn)
+  expect(getByTestId("ruler-label")).toHaveTextContent("Ruler off")
+  })
+  it("Each button in the navbar needs to display the correct text", () => {
+      render(<EditorFooter />);
+    
+      const zoomOutElement = screen.getByTestId("zoom-out-btn");
+      const zoomInElement = screen.getByTestId("zoom-in-btn");
+      const helpElement = screen.getByTestId("help-btn");
+    
+      expect(zoomOutElement).toBeInTheDocument();
+      expect(zoomInElement).toBeInTheDocument();
+      expect(helpElement).toBeInTheDocument();
+    })
+})
+// test("Each button in the navbar needs to display the correct text", () => {
+//   render(<EditorFooter />);
+
+//   const zoomOutElement = screen.getByTestId("zoom-out-btn");
+//   const zoomInElement = screen.getByTestId("zoom-in-btn");
+//   const helpElement = screen.getByTestId("help-btn");
+
+//   expect(zoomOutElement).toBeInTheDocument();
+//   expect(zoomInElement).toBeInTheDocument();
+//   expect(helpElement).toBeInTheDocument();
+// });
--- src/components/EditorFooter/index.tsx
+++ src/components/EditorFooter/index.tsx
@@ -1,17 +1,22 @@
-import { Box, Flex, IconButton, FormControl, Switch, FormLabel,} from "@chakra-ui/react";
+import React from "react";
+import { Box, Flex, IconButton, FormControl, Switch, FormLabel } from "@chakra-ui/react";
 import { HiOutlineZoomOut, HiOutlineZoomIn } from "react-icons/hi";
 import { BiHelpCircle } from "react-icons/bi";
 
 const EditorFooter = () => {
+  const [rulerLabel, setRulerLabel] = React.useState("Ruler on");
   return (
     <Flex
-      flexDirection="row"
+      position="fixed"
+      bottom="0"
       justifyContent="space-between"
       alignItems="center"
       px={4}
       w="100%"
       bg="white"
+      boxShadow="xs"
       lineHeight="34px"
+      zIndex={-1}
     >
       <Box ml="96px">
         <IconButton
@@ -19,28 +24,40 @@ const EditorFooter = () => {
           icon={<HiOutlineZoomOut />}
           variant="editorFooterIconBtn"
           color="brand.primary"
+          data-testid="zoom-out-btn"
         />
         <IconButton
           aria-label="Forward edit"
           icon={<HiOutlineZoomIn />}
           variant="editorFooterIconBtn"
           color="brand.primary"
+          data-testid="zoom-in-btn"
         />
       </Box>
       <Flex gap={8}>
         <FormControl display="flex" alignItems="center">
-          <FormLabel htmlFor="ruler-swith-btn" mb="0">
-            Ruler On
+          <FormLabel htmlFor="ruler-swith-btn" mb="0" color="brand.primary" data-testid="ruler-label">
+            {rulerLabel}
           </FormLabel>
-          <Switch id="email-alerts" colorScheme="footerSwitch" sx={{"span:first-child":{
-            bgColor:"brand.primary",
-          }}}/>
+          <Switch
+            id="email-alerts"
+            colorScheme="footerSwitch"
+            sx={{
+              "span:first-child": {
+                bgColor: "brand.primary",
+              },
+            }}
+            checked
+            onChange={(e) => setRulerLabel(e.target.checked ? "Ruler on" : "Ruler off")}
+            data-testid="switch-btn"
+          />
         </FormControl>
         <IconButton
           aria-label="Forward edit"
           icon={<BiHelpCircle />}
           variant="editorFooterIconBtn"
           color="brand.primary"
+          data-testid="help-btn"
         />
       </Flex>
     </Flex>
