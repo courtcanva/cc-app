a4d9e207884b129f545ee0595d382b16e2903fe4
stkwn <34761@qq.com>
2022-05-21 16:41:54 +1000

    feat: cc-0017 update theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,6 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 import { StyleFunctionProps } from "@chakra-ui/theme-tools";
 import buttonStyles from "./components/buttonStyles";
-import priceBarDrawer, { DrawerButton } from "./components/priceBarDrawer";
 
 const config: ThemeConfig = {
   initialColorMode: "light",
@@ -24,16 +23,17 @@ const colors = {
     secondary: "#40B484",
     primaryLight: "#496A80",
   },
-  background: {
-    primary: "#2C3333",
-  },
-  fontcolor: {
-    primary: "#C8C5C5",
-  },
   CourtSizecolor: {
     btc: "#40B484",
     bt: "#FFFFFF",
     border: "#344C5C"
+  },
+  background:{
+    primary : "#2C3333",
+    secondary : "#4f5757",
+  },
+  fontcolor:{
+    primary : "#C8C5C5",
   },
   footerSwitch:{
     500:"#8FE9C3",
@@ -53,8 +53,6 @@ const components = {
     }),
   },
   Button: { ...buttonStyles },
-  Drawer: { ...priceBarDrawer },
-  DrawerButton,
 };
 
 const theme = extendTheme({ colors, config, styles, components});
