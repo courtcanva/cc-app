ff1a289f56cdbdf8ec2796d1117427f6e1ff9cf6
korn <zk930602@gmail.com>
2022-05-21 16:39:56 +1000

    feat: cc-0018 finished 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,27 +1,44 @@
-import NavigationBar from "@/components/NavBar";
-import Link from "next/link";
-import { Flex, Button, Text } from "@chakra-ui/react";
+import Link from 'next/link'
+import { Flex, Button, Text } from "@chakra-ui/react"
 import UnSmile from "@/assets/svg/404.svg";
-import HOME_PAGE_LINK from "@/constants";
+import HOME_PAGE_LINK from '@/constants';
+import NextHeadSeo from "next-head-seo";
 
-export default function Custon404() {
-  return (
-    <>
-      {/* <NavigationBar/> */}
-      <Flex direction="column" justify="center" align="center">
-        <UnSmile />
-        <Text fontWeight="bold" fontSize="8xl">
-          404
-        </Text>
-        <Text fontWeight="bold" fontSize="xl">
-          Oops!Something went wrong
-        </Text>
-        <Link href={HOME_PAGE_LINK} passHref>
-          <Button mt="1rem" variant="shareBtn" pl="2.5rem" pr="2.5rem">
-            Take me back to homepage
-          </Button>
-        </Link>
-      </Flex>
-    </>
-  );
+export default function Custom404() {
+    return (
+        <>
+            <NextHeadSeo
+                title={`404 Error page - Courtcanva`}
+                description={`404 Error page on CourtCanva`}
+                canonical={`http://www.courtcanva.com/404`}
+            />
+
+            <Flex direction="column" justify="center" align="center" h="calc(100vh - 106px)">
+            <UnSmile />
+            <Text
+                fontWeight="bold"
+                fontSize="8xl"
+            >
+                404
+            </Text>
+            <Text
+                fontWeight="bold"
+                fontSize="xl"
+            >
+                Oops!Something went wrong
+            </Text>
+            <Link href={HOME_PAGE_LINK} passHref>
+                <Button
+                    mt="1rem"
+                    pl="2.5rem"
+                    pr="2.5rem"
+                    variant="shareBtn"
+                >Take me back to homepage
+                </Button>
+            </Link>
+        </Flex>
+        </>
+    )
 }
+
+
