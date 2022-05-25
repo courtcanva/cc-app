2505c099e70d24e573ae51ac8c20fee3a3fb1a61
olivia <oliviaxu.devops@gmail.com>
2022-05-25 22:44:56 +1000

    feat: cc-0028 removed unused import from chakra-ui library

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -4,12 +4,8 @@ import {
   Popover,
   PopoverTrigger,
   PopoverContent,
-  PopoverHeader,
   PopoverBody,
-  PopoverFooter,
   PopoverArrow,
-  PopoverCloseButton,
-  PopoverAnchor,
 } from "@chakra-ui/react";
 
 import ColorPlate from "./colorPlate";
