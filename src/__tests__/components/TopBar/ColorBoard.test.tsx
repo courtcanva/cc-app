21b89aeac84c9d070a6200edd0f5d16f4dbceb74
olivia <oliviaxu.devops@gmail.com>
2022-05-26 16:38:37 +1000

    feat: cc-0028 change for loop to ES6 for of

--- src/__tests__/components/TopBar/ColorBoard.test.tsx
+++ src/__tests__/components/TopBar/ColorBoard.test.tsx
@@ -12,8 +12,13 @@ describe("ColorBoard", () => {
   it("Should render colors correctly", () => {
     render(<ColorBoard />);
 
-    for (let index = 0; index < mockPlateColors.length; index++) {
-      const color = mockPlateColors[index];
+    // for (let index = 0; index < mockPlateColors.length; index++) {
+    //   const color = mockPlateColors[index];
+    //   const colorElement = screen.getByTestId(color);
+    //   expect(colorElement).toHaveStyle({ "background-color": color });
+    // }
+
+    for (const color of mockPlateColors) {
       const colorElement = screen.getByTestId(color);
       expect(colorElement).toHaveStyle({ "background-color": color });
     }
