db0425a94be41db2cb41fefc3e3d746d3c199a14
olivia <oliviaxu.devops@gmail.com>
2022-05-26 16:43:55 +1000

    feat: cc-0028 removed commented code

--- src/__tests__/components/TopBar/ColorBoard.test.tsx
+++ src/__tests__/components/TopBar/ColorBoard.test.tsx
@@ -12,12 +12,6 @@ describe("ColorBoard", () => {
   it("Should render colors correctly", () => {
     render(<ColorBoard />);
 
-    // for (let index = 0; index < mockPlateColors.length; index++) {
-    //   const color = mockPlateColors[index];
-    //   const colorElement = screen.getByTestId(color);
-    //   expect(colorElement).toHaveStyle({ "background-color": color });
-    // }
-
     for (const color of mockPlateColors) {
       const colorElement = screen.getByTestId(color);
       expect(colorElement).toHaveStyle({ "background-color": color });
