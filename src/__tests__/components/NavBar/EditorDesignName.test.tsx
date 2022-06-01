62b1b7412fe8ab9260d15cd4c91212db5f604fa0
Bowen <93563222+BowenLi61@users.noreply.github.com>
2022-06-01 12:58:45 +0800

    Feature/cc-0029 a user can rename the design (#37)
    * feat: cc-0029 first version
    
    * feat: cc-0029 get the value
    
    * feat: cc-0029 update the test file and margin of the input
    
    * feat: cc-0029 test file change back
    
    * feat: cc-29 Align icon and text and padding
    
    * feat: cc-0029 align icon and text fix padding
    
    * feat: cc-0029 update the test file
    
    Co-authored-by: stkwn <34761@qq.com>

---
+++ src/__tests__/components/NavBar/EditorDesignName.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen} from "@testing-library/react";
+import EditorDesignName from "../../../components/NavBar/EditorDesignName";
+import userEvent from '@testing-library/user-event';
+
+describe("EditorDesignName", () => {
+  test("click the btn can show the input element", () => {
+    render(<EditorDesignName />);
+    const btnElement = screen.getByLabelText("Edit");
+    const spanElement = screen.getByText("Court Canva 1");
+    const inputElement = screen.getByDisplayValue("Court Canva 1");
+
+    userEvent.click(btnElement);
+    expect(spanElement).toHaveAttribute("hidden");
+    expect(inputElement).not.toHaveAttribute("hidden");
+
+    userEvent.type(inputElement, 'new design name')
+    expect(inputElement).toHaveValue('new design name')
+
+    userEvent.click(inputElement);
+    expect(spanElement.hidden).toBeTruthy();
+    expect(inputElement.hidden).toBeFalsy();
+  })
+});
---
+++ src/components/NavBar/EditorDesignName.tsx
@@ -0,0 +1,48 @@
+import {
+  Flex,
+  IconButton,
+  useEditableControls,
+  Editable,
+  EditablePreview,
+  Input,
+  EditableInput,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+import { BiStar, BiPencil } from "react-icons/bi";
+
+const DesignName = () => {
+  const [value, setValue] = useState("Court Canva 1");
+  const EditableControls = () => {
+    const { isEditing, getEditButtonProps } = useEditableControls();
+    return isEditing ? null : (
+      <IconButton
+        aria-label="Edit"
+        icon={<BiPencil />}
+        variant="navbarIconBtn"
+        {...getEditButtonProps()}
+      />
+    );
+  };
+
+  return (
+    <Flex justifyContent="center" alignItems="center" fontSize="xl">
+      <Editable
+        color="white"
+        textAlign="center"
+        isPreviewFocusable={false}
+        onChange={(value) => setValue(value)}
+        value={value}
+        display="flex"
+        alignItems="center"
+      >
+        <IconButton aria-label="Star" icon={<BiStar />} variant="navbarIconBtn" />
+        <EditablePreview p="0px 8px" />
+        <Input as={EditableInput} />
+        <EditableControls />
+      </Editable>
+    </Flex>
+  );
+};
+
+export default DesignName;
