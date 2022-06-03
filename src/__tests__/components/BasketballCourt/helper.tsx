75509a8a2e2ee664e25ec4ca3ac9b3a0a1369e8e
JIN <51282481+Fuchih@users.noreply.github.com>
2022-06-03 16:26:59 +1000

    fix: cc-0021 fixed any type

--- src/__tests__/components/BasketballCourt/helper.tsx
+++ src/__tests__/components/BasketballCourt/helper.tsx
@@ -2,16 +2,21 @@ import Konva from "konva";
 import React from "react";
 import ReactDOM from "react-dom";
 
-const render = async (component: React.ReactChild) => {
+type Component = React.ReactNode
+interface AppProps {
+  onUpdate: (prop: unknown) => void;
+  children: Component;
+}
+
+const render = async (component: Component) => {
   const node = document.createElement("div");
   const root = document.body.appendChild(node);
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const App = ({ onUpdate, children }: any) => {
+  const App = ({ onUpdate, children }: AppProps) => {
     React.useEffect(() => {
       onUpdate(null);
     });
-    return children;
+    return children as JSX.Element;
   };
 
   await new Promise((resolve) => {
@@ -20,7 +25,7 @@ const render = async (component: React.ReactChild) => {
 
   return {
     stage: Konva.stages[Konva.stages.length - 1],
-    rerender: async (component: React.ReactNode) => {
+    rerender: async (component: Component) => {
       await new Promise((resolve) => {
         ReactDOM.render(<App onUpdate={resolve}>{component}</App>, root);
       });
