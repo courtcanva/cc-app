63ab94687119e21f575f06a1e769a283eed72a6a
korn <zk930602@gmail.com>
2022-06-09 11:39:08 +1000

    feat: cc-0023 add unit test

---
+++ src/__tests__/redux/courtSlices/topKeyArea.test.tsx
@@ -0,0 +1,16 @@
+import reducer, {
+    changeStartPoint,
+    initialState,
+    TopKeyAreaState,
+} from "@/store/reducer/courtSlices/topKeyAreaSlice";
+import store from "@/store/index";
+
+it("should return the initial state", () => {
+    const state = store.getState().topKeyArea;
+    expect(state).toEqual(initialState);
+});
+
+it("should handle coordinates", () => {
+    const previousState: TopKeyAreaState = initialState;
+    expect(reducer(previousState, changeStartPoint())).toEqual(initialState);
+});
