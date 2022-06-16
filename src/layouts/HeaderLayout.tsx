853667139ed9c0408053ebf11bb9d2c7c3b091e0
Biyu Zhou <beryl.zhou815@gmail.com>
2022-06-16 20:06:44 +1000

    fix: cc-0027 delete easy-in-out effect

--- src/layouts/HeaderLayout.tsx
+++ src/layouts/HeaderLayout.tsx
@@ -1,17 +1,10 @@
 import { ReactNode } from "react";
-import { motion } from "framer-motion";
 import Head from "next/head";
 
 interface HeaderLayoutProps {
   children: ReactNode;
   title?: string;
 }
-
-const variants = {
-  hidden: { opacity: 0, x: 0, y: 20 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: -0, y: 20 },
-};
 
 // pass title value to set the head title， e.g. In 404 page, write <HeaderLayout title="404"></HeaderLayout>
 const HeaderLayout: React.FC<HeaderLayoutProps> = ({ children, title }) => {
