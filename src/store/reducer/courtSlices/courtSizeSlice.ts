76559557871bde2fd6e0c7157f7cbe2579df3456
Biyu Zhou <beryl.zhou815@gmail.com>
2022-06-18 20:18:25 +1000

    fix: cc-0027 update canva position

--- src/store/reducer/courtSlices/courtSizeSlice.ts
+++ src/store/reducer/courtSlices/courtSizeSlice.ts
@@ -7,7 +7,7 @@ export interface courtSizeState {
 
 export const initialState: courtSizeState = {
   initPointX: 75,
-  initPointY: 65,
+  initPointY: 75,
   courtAreaXLength: 1400,
   courtAreaYLength: 1500,
   threePointLineToCourtEdgeLenth: 90,
