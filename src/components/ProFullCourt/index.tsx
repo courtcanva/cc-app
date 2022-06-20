c1b6be9bb65f33350be63a38b594b04d9afaf64f
Biyu Zhou <beryl.zhou815@gmail.com>
2022-06-20 22:23:36 +1000

    feat: cc-0051 second version

--- src/components/ProFullCourt/index.tsx
+++ src/components/ProFullCourt/index.tsx
@@ -1,3 +1,4 @@
+import {useEffect, useState} from "react";
 import { Stage, Layer, Group } from "react-konva";
 import { Flex } from "@chakra-ui/react";
 import { ReactReduxContext, Provider } from "react-redux";
@@ -10,21 +11,40 @@ import Border from "../BasketballCourt/Border";
 import BorderDimensionLine from "../BasketballCourt/BorderDimensionLine";
 import ArrowLine from "../BasketballCourt/Arrow";
 import { useStoreSelector } from "@/store/hooks";
+import React from "react";
 
 const ProFullCourt = () => {
+
   const { courtAreaXLength, courtAreaYLength } = useStoreSelector((state) => state.courtSize);
+  const [size, setSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+  useEffect(() => {
+    const checkSize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        
+      });
+    };
+    
+    window.addEventListener('resize', checkSize);
+    return () => window.removeEventListener('resize', checkSize);
+
+  }, []);
 
   const stageMargin = 2500;
-  const convaWidth = courtAreaXLength * 2 + stageMargin * 2;
+  const convaWidth = courtAreaXLength * 2 + stageMargin * 2; // actual court size plus reserved margin size (prepare for 2m border)
   const convaHeight = courtAreaYLength + stageMargin * 2;
 
-  let stageHeight = window.innerHeight - 250;
+  let stageHeight : number;
+  (size.height >= 768) ? (stageHeight = size.height - 250) : (stageHeight = 768 - 250);
   let stageWidth = stageHeight * (convaWidth / convaHeight);
 
-  if ((window.innerHeight - 250) / (window.innerWidth - 118) > convaHeight / convaWidth) {
-    stageWidth = window.innerWidth - 118;
+  if ((size.height - 250) / (size.width - 118) > convaHeight / convaWidth) {
+    (size.width >= 768) ? (stageWidth = size.width - 118) : (stageWidth = 768 - 118);
     stageHeight = stageWidth * (convaHeight / convaWidth);
   }
+  
   const courtRatio = stageHeight / (courtAreaYLength + stageMargin * 2);
   const initPointX = stageWidth / 2 - courtAreaXLength * courtRatio;
   const initPointY = stageHeight / 2 - (courtAreaYLength * courtRatio) / 2;
@@ -36,8 +56,8 @@ const ProFullCourt = () => {
       left="98px"
       width="calc(100% - 98px)"
       height="calc(100% - 230px)"
-      minWidth={stageWidth}
-      minHeight={stageHeight}
+      minWidth={768 - 98}
+      minHeight={768 - 230}
       justifyContent="center"
       alignItems="center"
       margin="auto"
