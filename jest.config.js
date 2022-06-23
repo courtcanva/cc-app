86e9a002aeb13ef68f20030adf00ebc7b7c5a23b
Emily <mei.gong@icloud.com>
2022-06-23 21:04:31 +1000

    feat: cc-0047 create full court

--- jest.config.js
+++ jest.config.js
@@ -29,6 +29,8 @@ const customJestConfig = {
     "!<rootDir>/src/pages/_document.tsx",
     "!<rootDir>/src/utils/axios.ts",
     "!<rootDir>/src/components/BasketballCourt/*", // exclude canvas
+    "!<rootDir>/src/components/FullCourt/*", // exclude canvas
+    "!<rootDir>/src/components/ProFullCourt/*", // exclude canvas
     "!<rootDir>/src/store/reducer/counterSlice.ts", // should be deleted later
     "!<rootDir>/src/store/hooks.ts", // exclude redux hooks
     "!<rootDir>/node_modules/",
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import HeaderLayout from "../layouts/HeaderLayout";
 import dynamic from "next/dynamic";
 
 const ProFullCourt = dynamic(() => import("@/components/ProFullCourt"), { ssr: false });
+const FullCourt = dynamic(() => import("@/components/FullCourt"), { ssr: false });
 
 const Home: NextPage = () => {
   return (
     <HeaderLayout>
       <ProFullCourt />
+      <FullCourt />
     </HeaderLayout>
   );
 };
