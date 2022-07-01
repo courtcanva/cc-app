adc85d583d578450a6a72ddbdf4f6f67f16f0aa9
Jzeva <jzeva1213@gmail.com>
2022-07-01 23:08:50 +1000

    feat: cc-0043 fix sign out button after refresh

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -15,14 +15,14 @@ import { useStoreSelector } from "@/store/hooks";
 
 const NavigationBar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  let userInfo;
+  if(typeof window !== "undefined"){
+    userInfo = JSON.parse(localStorage.getItem("UserInfos")!)
+  }
   // TODO: Try using customHooks later:https://usehooks.com/useLocalStorage/
   /* istanbul ignore next */
   const [loginData, setLoginData] = useState(
-    typeof window !== "undefined"
-      ? localStorage.getItem("loginData")
-        ? JSON.parse(localStorage.getItem("loginData")!)
-        : null
-      : null
+    userInfo? userInfo : null
   );
 
   /* istanbul ignore next */
@@ -93,9 +93,7 @@ const NavigationBar = () => {
             ></MenuButton>
           </Menu>
         ) : (
-          <button onClick={handleLogout} style={{ color: "white" }}>
-            Sign out
-          </button>
+          <Button onClick={handleLogout}>Sign out</Button>
         )}
         <LoginModalContent
           isOpen={isOpen}
