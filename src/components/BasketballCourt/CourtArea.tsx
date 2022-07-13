ce1c1231037500d185a85798ec9e351cfe8ce476
Jenna Wan <101085792+jennawan@users.noreply.github.com>
2022-07-13 12:03:36 +1000

    fix: fix courtarea not shown in small and medium courts (#119)

--- src/components/BasketballCourt/CourtArea.tsx
+++ src/components/BasketballCourt/CourtArea.tsx
@@ -12,7 +12,13 @@ interface CourtAreaProps {
 
 const CourtArea: React.FC<CourtAreaProps> = ({ courtWidth, startPoint }) => {
   const { courtAreaYLength } = useStoreSelector((state) => state.courtSize);
-
+  let courtAreaOffset = 0;
+  // if width from backend <15k, change it 15k to ensure CourtArea render correctly
+  if (courtAreaYLength === 7000) {
+    courtAreaOffset = 4000;
+  } else if (courtAreaYLength === 5000) {
+    courtAreaOffset = 5000;
+  }
   const selectedColor = useStoreSelector((state) => state.courtColor.selectedColor);
   const courtAreaColor = getColor("courtArea");
   const dispatch = useDispatch();
@@ -26,7 +32,7 @@ const CourtArea: React.FC<CourtAreaProps> = ({ courtWidth, startPoint }) => {
       height={courtAreaYLength}
       fill={courtAreaColor}
       x={startPoint.X}
-      y={startPoint.Y}
+      y={startPoint.Y + courtAreaOffset}
       stroke="white"
       strokeWidth={courtWhiteLine}
       onClick={handleColorChange}
