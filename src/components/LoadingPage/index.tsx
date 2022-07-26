69df5040e979c32b5a0beba88cf3e1b644251d95
Arina_ke <34761@qq.com>
2022-07-26 12:21:41 +1000

    Feature/cc 0104 making a new design for lading (#146)
    * feat: cc-0104 update loadingpage
    
    * feat: cc-0104 update the loading page

--- src/components/LoadingPage/index.tsx
+++ src/components/LoadingPage/index.tsx
@@ -1,10 +1,23 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Image } from "@chakra-ui/react";
+
 
 export default function LoadingPage() {
   return (
-    <Flex direction="column" justify="center" align="center" height="100vh" width="100vw">
-      <Text fontWeight="bold" fontSize="72px">
-        Loading...
+    <Flex position="fixed"
+      top="123px"
+      left="98px"
+      width="calc(100% - 98px)"
+      height="calc(100% - 230px)"
+      margin="auto"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      >
+
+      <Image boxSize="87px" objectFit="cover" src="./android-chrome-512x512.png" alt="logo" >
+      </Image>
+      <Text color="brand.primary" fontSize="16px" paddingTop="33px">
+        Please wait while the court design is loading...
       </Text>
     </Flex>
   );
