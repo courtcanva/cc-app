6b662ace4d83443933db11c7cc3bc369152836db
JasonCaoAU <jason2019au@gmail.com>
2022-09-17 21:35:38 +0800

    feat: cc-0007 update the unit testing

--- src/__tests__/components/NavBar/ShoppingCart.test.tsx
+++ src/__tests__/components/NavBar/ShoppingCart.test.tsx
@@ -2,12 +2,19 @@ import { render, screen } from "@testing-library/react";
 import ShoppingCart from "@/components/NavBar/ShoppingCart";
 
 describe("ShoppingCart", () => {
-  const quantity = 5;
-  it("Should render the quantity and circle element successfully", () => {
-    const { getByText } = render(<ShoppingCart quantity={quantity} />);
+  const quantity1 = 5;
+  it("Should render the quantity '5' and circle element successfully", () => {
+    const { getByText } = render(<ShoppingCart quantity={quantity1} />);
 
     const quantityDivElement = screen.getByTestId("quantity-box");
     expect(quantityDivElement).toBeInTheDocument();
     expect(getByText(5)).toBeInTheDocument();
+  });
+
+  const quantity2 = 0;
+  it("the circle element should not be rendered", () => {
+    render(<ShoppingCart quantity={quantity2} />);
+    const quantityDivElement = screen.queryByTestId("quantity-box");
+    expect(quantityDivElement).toBeNull();
   });
 });
