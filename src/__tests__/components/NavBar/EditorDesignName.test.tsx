de00456f27bb04b12a2dcf02c731d052c6adfb69
jennawan <wanj525@gmail.com>
2022-09-24 09:27:10 +1000

    test: add UI testing for EditorDesignName component

--- src/__tests__/components/NavBar/EditorDesignName.test.tsx
+++ src/__tests__/components/NavBar/EditorDesignName.test.tsx
@@ -21,4 +21,21 @@ describe("EditorDesignName", () => {
     expect(spanElement.hidden).toBeTruthy();
     expect(inputElement.hidden).toBeFalsy();
   });
+  test("show feedback modal when invalid name was input", () => {
+    renderWithMockedProvider(<EditorDesignName />);
+    const btnElement = screen.getByLabelText("Edit");
+    const inputElement = screen.getByDisplayValue("Court Canva 1");
+
+    userEvent.click(btnElement);
+    userEvent.type(inputElement, "new design name-!");
+    userEvent.click(document.body);
+    const feedbackModal = screen.getByRole("dialog");
+    expect(feedbackModal).toBeInTheDocument();
+    expect(screen.getByText(/not a valid name/i)).toBeInTheDocument();
+
+    const closeBtnEl = screen.getByText("Close");
+    userEvent.click(closeBtnEl);
+    expect(feedbackModal).not.toBeVisible();
+    expect(inputElement).toHaveValue("Court Canva 1");
+  });
 });
