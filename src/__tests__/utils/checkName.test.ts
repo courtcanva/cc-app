84b24fcb008451cd36c7b272ed545fb0f9ecdee0
Jenna Wan <101085792+jennawan@users.noreply.github.com>
2022-09-26 12:13:00 +1000

    Cc 10 validate input value when changing the design name (#181)
    * feat: add validation for design name and refactor code
    
    * refactor: refactor custome hook
    
    * test: add testing for custom hook useNameCheckFeedback
    
    * chore: change feedback message for invalid name
    
    * test: add UI testing for EditorDesignName component
    
    * chore: change component name of NameChangeFeedback to NameChangeAlertModal
    
    * refactor: extract validation rules from checkName to be setValidation

--- src/__tests__/utils/checkName.test.ts
+++ src/__tests__/utils/checkName.test.ts
@@ -1,13 +1,19 @@
-import checkName from "@/utils/checkName";
+import checkName, { setValidation } from "@/utils/checkName";
 
 describe("checkName is working", () => {
+  const validation = setValidation({ maxCharLength: 15, onlyWordChar: true });
   it("should return existed", () => {
-    const nameCheck = checkName("Pro Full Court", ["Pro Full Court"]);
+    const nameCheck = checkName("Pro Full Court", ["Pro Full Court"], validation);
     expect(nameCheck).toEqual("existed");
   });
 
   it("should return blank", () => {
-    const nameCheck = checkName("", ["Pro Full Court"]);
+    const nameCheck = checkName("", ["Pro Full Court"], validation);
     expect(nameCheck).toEqual("blank");
+  });
+
+  it("should return invalid", () => {
+    const nameCheck = checkName("court!", ["Pro Full Court"], validation);
+    expect(nameCheck).toEqual("invalid");
   });
 });
