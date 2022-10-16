6e75554777203bfb3138e3c651824b62364f9aed
JasonCaoAU <jason2019au@gmail.com>
2022-10-16 09:37:02 +0800

    feat: cc-0051 updated the unit testing in EditorSideBarContent.test.tsx file

--- src/__tests__/components/EditorSideBar/EditorSideBarContent.test.tsx
+++ src/__tests__/components/EditorSideBar/EditorSideBarContent.test.tsx
@@ -1,9 +1,10 @@
+import { screen } from "@testing-library/react";
 import renderWithMockedProvider from "../../utils";
 import EditorSideBarContent from "@/components/EditorSideBar/EditorSideBarContent";
 
 describe("EditorSideBarContent", () => {
   it("Should render closing button", () => {
-    const { getByRole } = renderWithMockedProvider(
+    renderWithMockedProvider(
       <EditorSideBarContent
         iconClickTitle="some title"
         onHandleCloseClick={() => {
@@ -11,6 +12,7 @@ describe("EditorSideBarContent", () => {
         }}
       />
     );
-    expect(getByRole("button")).toBeInTheDocument();
+    const buttonEel = screen.getByRole("button");
+    expect(buttonEel).toBeInTheDocument();
   });
 });
