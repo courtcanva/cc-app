5ae47a2ef8b1c747a03b3e30a241dd36fa889a86
Tato <jason.ma1904@gmail.com>
2022-10-17 11:38:05 +1100

    fix: testing coverage for canvasControlModel, checkName and pricebarSlice (#215)
    fix: testing coverage for checkName priceBar and half of tokenService
    
    Co-authored-by: Norris Wu <norris.wu.au@outlook.com>
    Co-authored-by: Robert Xu <kritomomo@users.noreply.github.com>
    
    Co-authored-by: Norris Wu <norris.wu.au@outlook.com>
    Co-authored-by: Robert Xu <kritomomo@users.noreply.github.com>

--- src/__tests__/utils/checkName.test.ts
+++ src/__tests__/utils/checkName.test.ts
@@ -1,7 +1,7 @@
 import checkName, { setValidation } from "@/utils/checkName";
 
 describe("checkName is working", () => {
-  const validation = setValidation({ maxCharLength: 15, onlyWordChar: true });
+  let validation = setValidation({ maxCharLength: 15 });
   it("should return existed", () => {
     const nameCheck = checkName("Pro Full Court", ["Pro Full Court"], validation);
     expect(nameCheck).toEqual("existed");
@@ -12,8 +12,26 @@ describe("checkName is working", () => {
     expect(nameCheck).toEqual("blank");
   });
 
-  it("should return invalid", () => {
+  it("should return passCheck", () => {
     const nameCheck = checkName("court!", ["Pro Full Court"], validation);
+    expect(nameCheck).toEqual("passCheck");
+  });
+
+  it("should return invalid by passing illegal name", () => {
+    validation = setValidation({ maxCharLength: 15, onlyWordChar: true });
+    const nameCheck = checkName("court!", ["Pro Full Court"], validation);
+    expect(nameCheck).toEqual("invalid");
+  });
+
+  it("should return invalid by passing canBeBlank", () => {
+    validation = setValidation({ maxCharLength: 15, canBeBlank: true });
+    const nameCheck = checkName("", ["Pro Full Court"], validation);
+    expect(nameCheck).toEqual("passCheck");
+  });
+
+  it("should return invalid by passing minCharLength", () => {
+    validation = setValidation({ maxCharLength: 15, minCharLength: 2 });
+    const nameCheck = checkName("a", ["Pro Full Court"], validation);
     expect(nameCheck).toEqual("invalid");
   });
 });
