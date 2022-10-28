c79212b7591e62156adc7a8fb6c8ddb42d1f1842
wwwseven <102560176+wwwseven@users.noreply.github.com>
2022-10-28 14:28:45 +0800

    Feature/cc 59 order generation (#222)
    * feat: complete order generation page
    
    * test: add tests and update file structure
    
    * fix: add width
    
    * fix: update css and add responsive
    
    * fix: delete max width and correct typo
    
    * fix: add tooltip for buttons
    
    * fix: update data structure for order

---
+++ src/interfaces/order.d.ts
@@ -0,0 +1,16 @@
+import { IDesign } from "@/interfaces/design";
+import { PriceBar } from "@/store/reducer/priceBarSlice";
+
+interface IOrderItem {
+  design: IDesign;
+  quotation: string;
+  quotationDetails: PriceBar[];
+  image: string;
+  constructionDrawing: string;
+}
+
+export interface IOrder {
+  user_id: string;
+  items: IOrderItem[];
+  depositRatio: number;
+}
