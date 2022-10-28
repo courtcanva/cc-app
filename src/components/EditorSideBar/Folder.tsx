34c6ad359805f807aa6d183a99905ce02d85e167
Evelyn Zeng <zimantsang@gmail.com>
2022-10-29 00:55:46 +1100

    rebase main branch: cc-0060

--- src/components/EditorSideBar/Folder.tsx
+++ src/components/EditorSideBar/Folder.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Tag, Flex, Box } from "@chakra-ui/react";
 import { useDispatch } from "react-redux";
 import React, { useEffect, useState } from "react";
 import { AreaTileQty, changeCourtType } from "@/store/reducer/areaTileQtySlice";
@@ -7,8 +7,11 @@ import { mockTileData } from "../MockData/MockTileData";
 import { useStoreSelector } from "@/store/hooks";
 import { changeWholeCourtColor } from "@/store/reducer/tileSlice";
 import { ActionCreators } from "redux-undo";
+import FolderDeleteModal from "./Folder/FolderDeleteModal";
+import Image from "next/image";
+import { upLoadScreenshot } from "@/utils/manageExternalImage";
 
-const Folder: React.FC = () => {
+const Folder = () => {
   const dispatch = useDispatch();
   const { designsData, activeCourt } = useStoreSelector((state) => state.courtSpecData);
   const [activateDesign, setActivateDesign] = useState<string>(activeCourt.courtId);
@@ -37,26 +40,64 @@ const Folder: React.FC = () => {
   return (
     <Box height="100%" className="scrollbox">
       {designsData.map((design) => {
-        const { courtId, courtName, designName } = design;
+        const { courtId, courtName, designName, createdAt } = design;
+        const createDate = [
+          createdAt.slice(8, 10),
+          createdAt.slice(5, 7),
+          createdAt.slice(0, 4),
+        ].join("/");
+
+        // list
         return (
-          <Box
+          <Flex
             key={courtId}
             width="230px"
-            height="25px"
-            background="transparent"
-            marginBottom="18px"
-            display="flex"
+            height="160px"
+            background="white"
+            marginBottom="8px"
             alignItems="center"
-            justifyContent="left"
+            justifyContent="space-between"
             cursor="pointer"
-            fontSize="14"
+            fontSize="12"
+            color="black"
+            flexDirection="column"
             onClick={() => handleCourtSelecting(courtId)}
             data-testid={courtId}
             _hover={{ border: "2px solid button.hover" }}
             opacity={!activateDesign || activateDesign === courtId ? "1" : "0.4"}
           >
-            {designName} - {courtName}
-          </Box>
+            <Flex flexDirection="row" alignItems="flex-start" justifyContent="space-between">
+              <Tag>
+                {designName}
+                <br />
+                created at {createDate}
+              </Tag>
+              <Box
+                style={{
+                  backgroundColor: "#C13D46",
+                  height: "25px",
+                  width: "25px",
+                  borderRadius: "20%",
+                  margin: "5px",
+                }}
+              >
+                <FolderDeleteModal />
+              </Box>
+            </Flex>
+            <Box style={{ color: "black" }}>image</Box>
+            <Flex flexDirection="row" justifyContent="space-between">
+              <Tag
+              // variant="courtIDTag"
+              >
+                {courtName}
+              </Tag>
+              <Tag
+              // variant="courtTypeTag"
+              >
+                Basketball
+              </Tag>
+            </Flex>
+          </Flex>
         );
       })}
     </Box>
