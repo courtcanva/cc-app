11b0ceef69205a34a4e781a577973626ec3f45be
Double-Twelve <76163407+Double-Twelve@users.noreply.github.com>
2022-10-29 14:34:48 +1100

    refactor: make error messages consistent when design name is incorrect (#221)
    * refactor: make error messages consistent when design name is incorrect
    
    * fix: refactor the alert and check-design-name
    
    * fix: fix check-design-name issue
    
    Co-authored-by: wwwseven <102560176+wwwseven@users.noreply.github.com>

---
+++ src/components/TopBar/SaveAlert.tsx
@@ -0,0 +1,80 @@
+import {
+  AlertDialogCloseButton,
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogOverlay,
+  AlertDialogFooter,
+  Input,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import React, { useRef } from "react";
+
+const SaveAlert = ({
+  dialogOpen,
+  useDesignName,
+  handleCheckName,
+  nameCheck,
+  handleSaveAsDesign,
+  setDialogOpen,
+  setDesignName,
+  setNameCheck,
+}: any) => {
+  const cancelRef = useRef(null);
+  const close = () => {
+    setDialogOpen(false);
+    setDesignName(useDesignName);
+    setNameCheck("");
+  };
+
+  return (
+    <AlertDialog
+      motionPreset="slideInBottom"
+      leastDestructiveRef={cancelRef}
+      onClose={close}
+      isOpen={dialogOpen}
+      isCentered
+    >
+      <AlertDialogOverlay />
+      <AlertDialogContent>
+        <AlertDialogHeader>Save As</AlertDialogHeader>
+        <AlertDialogCloseButton />
+        <AlertDialogBody>
+          Your design will be saved in FOLDER.
+          <Text margin="5px 0">Court Name: {useDesignName}</Text>
+          <Input
+            marginTop="5px"
+            value={useDesignName}
+            onChange={handleCheckName}
+            placeholder="Make your unique court name."
+          />
+          <Text color="crimson" marginTop="0.2rem" fontSize="0.8rem">
+            {nameCheck}
+          </Text>
+        </AlertDialogBody>
+        <AlertDialogFooter>
+          <Button ref={cancelRef} onClick={close} w="75px">
+            Cancel
+          </Button>
+          <Button
+            bg="button.hover"
+            color="fontcolor.primary"
+            w="75px"
+            ml={3}
+            disabled={nameCheck !== ""}
+            onClick={handleSaveAsDesign}
+            ref={cancelRef}
+            _hover={{ bg: "brand.secondary", opacity: "0.60" }}
+            _active={{ bg: "brand.secondary", opacity: "0.60" }}
+          >
+            Save
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default SaveAlert;
