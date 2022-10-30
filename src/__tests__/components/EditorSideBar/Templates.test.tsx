3eb7c5bf985de76ff62252d7410ca340f86b4dd7
Zack Chen <zackchen5813@gmail.com>
2022-10-30 15:09:20 +1100

    feat: empty template list notice

--- src/__tests__/components/EditorSideBar/Templates.test.tsx
+++ src/__tests__/components/EditorSideBar/Templates.test.tsx
@@ -3,8 +3,14 @@ import { mockTemplateDataRaw } from "@/components/MockData/MockTemplateData";
 import { screen } from "@testing-library/dom";
 import renderWithMockedProvider from "../../utils";
 
-describe("Render the templates", () => {
-  test("The templates is correctly rendered", () => {
+describe("Render the templates list", () => {
+  test("Notice of empty template list is correct rendered", () => {
+    renderWithMockedProvider(<Templates templatesData={[]} />);
+    const emptyList = screen.getByText(/the template list is empty/i);
+    expect(emptyList).toBeInTheDocument();
+  });
+
+  test("All templates are correctly rendered", () => {
     renderWithMockedProvider(<Templates templatesData={mockTemplateDataRaw} />);
     const imgElements = screen.getAllByRole("img");
     expect(imgElements.length).toBe(3);
--- src/components/EditorSideBar/Templates.tsx
+++ src/components/EditorSideBar/Templates.tsx
@@ -1,5 +1,5 @@
 import { ITemplateDataDb } from "@/interfaces/template";
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import TemplateItem from "../TemplateList/TemplateItem";
 
 interface Props {
@@ -7,11 +7,13 @@ interface Props {
 }
 
 const Templates = (prop: Props) => {
+  const templateEmpty = prop.templatesData?.length === 0;
   return (
     <Box height="100%" className="scrollbox">
       {prop.templatesData?.map((template) => {
         return <TemplateItem key={template._id} template={template} />;
       })}
+      {templateEmpty && <Text>The template list is empty</Text>}
     </Box>
   );
 };
