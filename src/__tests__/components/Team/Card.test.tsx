34af8a014b1304bb27e0c45a1d620cc87a635932
Derek Zhu <98071700+RedRe4per@users.noreply.github.com>
2022-11-02 15:40:12 +1100

    Cc 0037 team page improvement (#231)
    * feat: cc-0037-team-page-improvement
    
    * feat: 0037-team-page-improvement
    
    * fix: extra-image-delete
    
    * fix: css bugs

--- src/__tests__/components/Team/Card.test.tsx
+++ src/__tests__/components/Team/Card.test.tsx
@@ -3,18 +3,18 @@ import Card from "@/components/Team/Card";
 
 describe("Card", () => {
   const member = {
-    id: "1",
-    name: "Eason Li",
-    profileImg: "/teamProfileImages/eason-li.jpeg",
+    _id: "test-id",
+    name: "Test Name",
+    profileImgUrl: "https://test.s3.ap-southeast-2.amazonaws.com/test-path/test.jpg",
     role: "Developer",
-    linkedInUrl: "https://www.linkedin.com/in/eason-li-dev/",
-    githubUrl: "https://github.com/GitHubEason",
-    emailAddress: "easonli0216@gmail.com",
+    linkedInUrl: "https://www.linkedin.com/in/test/",
+    githubUrl: "https://github.com/test",
+    emailAddress: "test@gmail.com",
   };
 
   it("Should render the property successfully", () => {
-    const { getByText } = render(<Card key={member.id} member={member} />);
+    const { getByText } = render(<Card key={member._id} member={member} />);
 
-    expect(getByText(/Eason Li/i)).toBeInTheDocument();
+    expect(getByText(/test name/i)).toBeInTheDocument();
   });
 });
--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,11 +1,33 @@
-import { SimpleGrid, Center, Text, Button } from "@chakra-ui/react";
+/* eslint-disable require-jsdoc */
+import { SimpleGrid, Center, Text, Button, Box } from "@chakra-ui/react";
 import NextHeadSeo from "next-head-seo";
 import Card from "@/components/Team/Card";
-import { memberList } from "@/components/Team/memberList";
 import Link from "next/link";
 import { environment } from "@/constants/environment";
+import { IGroupedMembers } from "@/interfaces/team";
+import { IMember } from "@/interfaces/team";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const Team = () => {
+interface Props {
+  groupedTeamMembers: GroupedTeamMembers;
+}
+
+interface GroupedTeamMembers {
+  [key: string]: IMember[];
+}
+
+export default function Team({ groupedTeamMembers }: Props) {
+  const CarouselIndicator = (onClickHandler: any, isSelected: boolean, index: number) => {
+    const defStyle = { marginLeft: 20, color: "lightgrey", cursor: "pointer", fontWeight: "bold" };
+    const style = isSelected ? { ...defStyle, color: "#2C4E8A" } : { ...defStyle };
+    return (
+      <span onClick={onClickHandler} style={style}>
+        {index + 1}
+      </span>
+    );
+  };
+
   return (
     <>
       <NextHeadSeo
@@ -13,24 +35,78 @@ const Team = () => {
         description={`Introduction and contact information of team members of CourtCanva`}
         canonical={`http://design.courtcanva.com/team`}
       />
-      <Center paddingY="80px" flexDirection="column">
-        <Text fontSize="32px" fontWeight="bold">
+      <Center paddingY="85px" flexDirection="column" overflow="hidden">
+        <Text fontSize="32px" fontWeight="bold" marginTop="-20px">
           Meet the Team
         </Text>
-        <SimpleGrid columns={{ base: 2, lg: 3 }} spacing="20px" marginY="30px">
-          {memberList.map(function (member) {
-            const { id } = member;
-            return <Card key={id} member={member} />;
+        <Carousel
+          showStatus={false}
+          showArrows={false}
+          emulateTouch={true}
+          autoPlay={true}
+          infiniteLoop={true}
+          renderIndicator={CarouselIndicator}
+        >
+          {Object.keys(groupedTeamMembers).map((key) => {
+            return (
+              <Center key={key} marginX="60px" paddingY="40px" flexDirection="column">
+                <Text fontSize="18px" fontWeight="bold" color="#2C4E8A">
+                  {key}
+                </Text>
+                <SimpleGrid columns={{ base: 2, lg: 3 }} spacing="16px" marginY="25px">
+                  {groupedTeamMembers[key].map((member) => {
+                    return <Card key={member._id} member={member} />;
+                  })}
+                </SimpleGrid>
+              </Center>
+            );
           })}
-        </SimpleGrid>
+        </Carousel>
         <Link href={environment.designURL as string} passHref>
-          <Button mt="1rem" pl="2.5rem" pr="2.5rem" variant="shareBtn">
+          <Button paddingX="2.5rem" variant="shareBtn">
             Take me back to homepage
           </Button>
         </Link>
       </Center>
     </>
   );
-};
+}
 
-export default Team;
+export async function getStaticProps() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URI}/team-member?isGrouped=true`);
+  const teamMembers: IGroupedMembers = await res.json();
+
+  const groupedTeamMembers: GroupedTeamMembers = {};
+  const maxCardQty = 9;
+  let mergeKey = "";
+
+  Object.keys(teamMembers).forEach((key) => {
+    if (teamMembers[key].length > maxCardQty) {
+      for (let i = 0; i < teamMembers[key].length; i = i + maxCardQty) {
+        const keyName = `${key} ${i / maxCardQty + 1}/${Math.ceil(
+          teamMembers[key].length / maxCardQty
+        )}`;
+        groupedTeamMembers[keyName] = teamMembers[key].slice(i, i + maxCardQty);
+      }
+    } else if (teamMembers[key].length <= maxCardQty / 2) {
+      if (mergeKey === "") {
+        mergeKey = key;
+        groupedTeamMembers[key] = teamMembers[key];
+      } else {
+        const keyName = `${mergeKey} & ${key}`;
+        groupedTeamMembers[keyName] = [...groupedTeamMembers[mergeKey], ...teamMembers[key]];
+        delete groupedTeamMembers[mergeKey];
+        mergeKey = "";
+      }
+    } else {
+      groupedTeamMembers[key] = teamMembers[key];
+    }
+  });
+
+  return {
+    props: {
+      groupedTeamMembers,
+    },
+    revalidate: 86400,
+  };
+}
