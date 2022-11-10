5dbf59c90401b6e44134e54237f24c97c51a3a83
Evelyn Zeng <zimantsang@gmail.com>
2022-11-10 20:44:48 +1100

    fix: remove comments

--- src/components/EditorSideBar/Folder.tsx
+++ src/components/EditorSideBar/Folder.tsx
@@ -12,19 +12,11 @@ import FolderListItem from "../FolderList/FolderListItems";
 const Folder = () => {
   const dispatch = useDispatch();
   const { designsData, activeCourt } = useStoreSelector((state) => state.courtSpecData);
-  console.log(designsData);
-  console.log("below is active");
-  console.log(activeCourt);
   const [activateDesign, setActivateDesign] = useState<string>(activeCourt.courtId);
   const { designTileList } = useStoreSelector((state) => state.designTileList);
 
-  // useEffect(() => {
-  //   if (designsData === undefined) return;
-  //   setActivateDesign(activeCourt.courtId);
-  //   console.log(activeCourt.courtId);
-  // }, [designsData]);
-
   const handleCourtSelecting = (courtId: string): void => {
+    console.log(activeCourt.courtId);
     setActivateDesign(courtId);
     dispatch(setActiveDesign(courtId));
     const selectedDesign = designsData.find((item) => item.courtId === courtId);
