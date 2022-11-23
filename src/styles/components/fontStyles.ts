cd2b158200e6acc8e5cde290610e83983faa889b
zhuocun <zhuocun.tang@outlook.com>
2022-11-23 23:01:03 +1100

    fix: fix css wrong format

--- src/styles/components/fontStyles.ts
+++ src/styles/components/fontStyles.ts
@@ -2,21 +2,18 @@ const fontStyles = {
   variants: {
     headerFont: {
       fontSize: { base: "20px", lg: "24px", xl: "32px" },
-      lineHeight: { base: "28px", lg: "32px", xl: "auto" },
       fontWeight: "700",
       textTransform: "capitalize",
       color: "brand.primary",
     },
     bodyFont: {
       fontSize: { base: "12px", lg: "16px", xl: "20px" },
-      lineHeight: "auto",
       fontWeight: "700",
       textTransform: "capitalize",
       color: "brand.primary",
     },
     textFont: {
       fontSize: { base: "10px", lg: "12px", xl: "16px" },
-      lineHeight: "auto",
       fontWeight: "700",
       color: "brand.primary",
     },
