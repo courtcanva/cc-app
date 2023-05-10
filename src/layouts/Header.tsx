12178919cd82c44cd25b646f6df987f71cac2ce9
mason <mason.yao@outlook.com>
2023-05-10 18:38:07 +1000

    remove uneccessary console log, remove missing icon from header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -7,7 +7,6 @@ const Header = () => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="CourtCanva" />
-        <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
