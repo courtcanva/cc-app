3ab7591407aa1357c4b89992c315b0a1f077da0d
mason <mason.yao@outlook.com>
2023-06-09 13:49:56 +1000

    test: optimize data structure of handlers in mockApi

--- src/__tests__/mockApi/handlers.ts
+++ src/__tests__/mockApi/handlers.ts
@@ -1,25 +1,27 @@
 import { rest } from "msw";
 
-const responseData = [
-  {
-    cementFloorPrice: 15000,
-    isDeleted: false,
-    createdAt: new Date(),
-    tilePrices: [
-      {
-        tile_id: "",
-        tileName: "mockName",
-        deliveryPrice: 1900,
-        price: { singleTone: 1200, twoTone: 1500, threeTone: 2000 },
-        isDeleted: false,
-      },
-    ],
-    updatedAt: new Date(),
-  },
-];
+const responseData = {
+  price: [
+    {
+      cementFloorPrice: 15000,
+      isDeleted: false,
+      createdAt: new Date(),
+      tilePrices: [
+        {
+          tile_id: "",
+          tileName: "mockName",
+          deliveryPrice: 1900,
+          price: { singleTone: 1200, twoTone: 1500, threeTone: 2000 },
+          isDeleted: false,
+        },
+      ],
+      updatedAt: new Date(),
+    },
+  ],
+};
 
 export const handlers = [
   rest.get(`${process.env.NEXT_PUBLIC_API_BASE_URI}/price`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(responseData));
+    return res(ctx.status(200), ctx.json(responseData.price));
   }),
 ];
