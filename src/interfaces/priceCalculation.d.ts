de4ff34c6f199d9934d0f99fba38d95b24e81a59
mason <mason.yao@outlook.com>
2023-06-15 14:26:14 +1000

    feat: add price bar to the right, decouple quotation calculation method with old component

--- src/interfaces/priceCalculation.d.ts
+++ src/interfaces/priceCalculation.d.ts
@@ -7,6 +7,12 @@ export interface TilePrices {
   tile_id: string;
   tileName: string;
   deliveryPrice: number;
-  price: number;
+  price: { singleTone: number; twoTone: number; threeTone: number };
   isDeleted: boolean;
+}
+
+export interface ITilePrice {
+  singleTone: number;
+  twoTone: number;
+  threeTone: number;
 }
