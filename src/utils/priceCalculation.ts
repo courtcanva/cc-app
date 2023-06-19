cca995cbcf2184a270872f30c7b71b2ffb819dc2
Mason-Yao <121526486+Mason-Yao@users.noreply.github.com>
2023-06-19 10:14:23 +0800

    Feat/cc 27 UI refactor quotation (#289)
    * feat: add price bar to the right, decouple quotation calculation method with old component
    
    * test: add tests for the component and calculation method
    
    * style: format the code
    
    * refactor: extracted duplicate code into a reusable function

---
+++ src/utils/priceCalculation.ts
@@ -0,0 +1,34 @@
+import { PriceBar } from "@/store/reducer/priceBarSlice";
+import { ITilePrice, TilePrices } from "@/interfaces/priceCalculation";
+
+export const calculateQuotation = (tileBlocks: PriceBar[], tilePrices: TilePrices) => {
+  const { price: tilePricesList, deliveryPrice } = tilePrices;
+  const tileQuote = calculateTile(tileBlocks, tilePricesList);
+  const deliveryQuote = calculateDelivery(tileBlocks, deliveryPrice);
+  return 1.1 * (tileQuote + deliveryQuote);
+};
+
+export const calculateDeposit = (totalQuote: number, depositRate: number) => {
+  return totalQuote * depositRate;
+};
+
+const calculateDelivery = (tileBlocks: PriceBar[], deliveryPrice: number) => {
+  let totalQuantity = 0;
+  for (const tile of tileBlocks) {
+    totalQuantity += tile.quantity;
+  }
+  return Math.ceil(totalQuantity / 1000) * (deliveryPrice / 100);
+};
+
+const calculateTile = (tileBlocks: PriceBar[], tilePricesList: ITilePrice) => {
+  const { singleTone, twoTone, threeTone } = tilePricesList;
+  const [singleToneNumber, twoToneNumber, threeToneNumber] = [1, 2, 3].map((length) =>
+    tileBlocks
+      .filter((item) => item.color.split(" ").length === length)
+      .map((tile) => tile.quantity)
+      .reduce((number, total) => number + total, 0)
+  );
+  return (
+    (singleTone * singleToneNumber + twoTone * twoToneNumber + threeTone * threeToneNumber) / 100
+  );
+};
