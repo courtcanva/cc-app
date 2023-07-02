e262c97438657c34525325aee3997df50a20d0fd
Howard-Cui <89081041+Howard-Cui@users.noreply.github.com>
2023-07-02 14:14:48 +1000

    feat: change the UI of the left top button

--- src/__tests__/components/NavBar/index.test.tsx
+++ src/__tests__/components/NavBar/index.test.tsx
@@ -2,6 +2,7 @@ import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import NavigationBar from "@/components/NavBar";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import renderWithMockedProvider from "../../utils";
+import userEvent from "@testing-library/user-event";
 
 describe("NavigationBar", () => {
   test("Each button in the navbar needs to display the correct text", () => {
@@ -11,35 +12,12 @@ describe("NavigationBar", () => {
       </GoogleOAuthProvider>
     );
 
-    const homeButtonElement = screen.getByText(/Home/i);
+    const homeButtonElement = screen.getByText(/CourtCanva/i);
     // const shareButtonElement = screen.getByText(/Share/i);
 
     expect(homeButtonElement).toBeInTheDocument();
     // expect(shareButtonElement).toBeInTheDocument();
-    expect(homeButtonElement).toHaveTextContent("Home");
+    expect(homeButtonElement).toHaveTextContent("CourtCanva");
     // expect(shareButtonElement).toHaveTextContent("Share");
   });
-
-  // it("Should render Share button", () => {
-  //   renderWithMockedProvider(
-  //     <GoogleOAuthProvider clientId="testid">
-  //       <NavigationBar />
-  //     </GoogleOAuthProvider>
-  //   );
-
-  //   const openButton = screen.getByTestId("share-btn");
-  //   expect(openButton).toBeInTheDocument();
-  // });
-
-  // it("Should render login Modal when click button", async () => {
-  //   renderWithMockedProvider(
-  //     <GoogleOAuthProvider clientId="testid">
-  //       <NavigationBar />
-  //     </GoogleOAuthProvider>
-  //   );
-  //   const shareButton = screen.getByTestId("share-btn");
-  //   fireEvent.click(shareButton);
-  //   const loginModalDialog = screen.getByRole("dialog");
-  //   await waitFor(() => expect(loginModalDialog).toBeVisible());
-  // });
 });
